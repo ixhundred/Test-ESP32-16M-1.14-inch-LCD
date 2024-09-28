6e58db01db4692259e5838cb6e5b060fadf6f95c
Vanlop Incham <ixhundred@hotmail.com>
2024-09-29 00:03:15 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,179 @@
+#include <Arduino.h>
+#include <EEPROM.h>
+#include <SPI.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#define TFT_CS         5
+#define TFT_RST        23
+#define TFT_DC         16
+//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+#define TFT_MOSI 19  // Data out
+#define TFT_SCLK 18  // Clock out
+#define TFT_BL   4
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+#define BT_L  0
+#define BT_R  35
+
+float p = 3.1415926;
+
+void testlines(uint16_t color);
+void testdrawtext(char *text, uint16_t color);
+void testfastlines(uint16_t color1, uint16_t color2);
+void tftPrintTest();
+
+void setup() {
+  Serial.begin(115200);
+  EEPROM.begin(4096);
+  randomSeed(analogRead(A0));
+  pinMode(TFT_BL,OUTPUT);
+  pinMode(BT_L,INPUT_PULLUP);
+  pinMode(BT_R,INPUT_PULLUP);
+  digitalWrite(TFT_BL,1);
+  
+
+  Serial.println(F("#Initialized Screen..."));
+  tft.init(135, 240);           // Init ST7789 240x135, 1.14 inch
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+  Serial.printf("Frame time = %d ms\r\n",time);
+  tft.setRotation(2);
+
+  // line draw test
+  testlines(ST77XX_YELLOW);
+  delay(500);
+
+  // optimized lines
+  testfastlines(ST77XX_RED, ST77XX_BLUE);
+  delay(500);
+
+  // optimized lines
+  tftPrintTest();
+  delay(4000);
+
+  tft.setTextSize(2);
+  testdrawtext("ixuhndred",ST77XX_BLUE);
+
+  Serial.println("#Ready");
+}
+
+void loop() {
+  if(digitalRead(BT_L)==LOW) {
+    delay(20);
+    if(digitalRead(BT_L)==LOW) {
+      Serial.println("#Left pressed");
+      while(digitalRead(BT_L)==LOW);
+      Serial.println("#Left released");
+    }
+  }
+  if(digitalRead(BT_R)==LOW) {
+    delay(20);
+    if(digitalRead(BT_R)==LOW) {
+      tft.invertDisplay(false);
+      Serial.println("#Right pressed");
+      while(digitalRead(BT_R)==LOW);
+      tft.invertDisplay(true);
+      Serial.println("#Right released");
+    }
+  }
+}
+
+void testlines(uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, 0, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, 0, 0, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+    delay(0);
+  }
+}
+
+void testdrawtext(char *text, uint16_t color) {
+  //tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void testfastlines(uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t y=0; y < tft.height(); y+=5) {
+    tft.drawFastHLine(0, y, tft.width(), color1);
+  }
+  for (int16_t x=0; x < tft.width(); x+=5) {
+    tft.drawFastVLine(x, 0, tft.height(), color2);
+  }
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
+}
